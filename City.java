2768ced3046598bf4cf88d2f3f97fb0aaedc339b
kebapci42 <168354657+kebapci42@users.noreply.github.com>
2024-12-16 23:41:04 +0300

    A constructor, and getter and setter methods are added into the City class

--- City.java
+++ City.java
@@ -1,5 +1,33 @@
 public class City {
-    public static String[] createCityArray(int size){
-        return new String[size];
+    public String name;
+    public double time;
+    public City previousCity;
+
+    public City(String name, double time) {
+        this.name = name;
+        this.time = time;
+        this.previousCity = null;
+    }
+
+    //
+    public String getName() {
+        return this.name;
+    }
+
+    // Getter and setter for time
+    public double getTime() {
+        return this.time;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
+
+    // Getter and setter for previous city
+    public City getPreviousCity() {
+        return this.previousCity;
+    }
+    public void setPreviousCity(City previousCity) {
+        this.previousCity = previousCity;
     }
 }
