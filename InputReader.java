bad68590af4e7b466f5a743e82ff72dd1471a540
kebapci42 <168354657+kebapci42@users.noreply.github.com>
2024-12-16 20:29:04 +0300

    In InputReader class; createCityArray, createRotueArray and createQuestionArray methods are added. The method createCityArray is removed from City class

--- InputReader.java
+++ InputReader.java
@@ -3,33 +3,38 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class InputReader {
+    public static String[] document;
     public static String[] cities;
-    public static String[] routes;
+    public static String[][] routes;
+    public static String[] question;
 
     public static void readInputFile(String fileName){
 
         Scanner reader = null;
-
+        
         try {
             
             reader = new Scanner(Paths.get(fileName));
             
-            String line;
-            for(int lineNumber = 0; reader.hasNextLine(); lineNumber++){
+            // Find the length of the document
+            int lineCount = 0;
 
-                line = reader.nextLine();
-
-                // Parse the size of city array
-                if (lineNumber == 0) {
-                    cities = City.createCityArray(Integer.parseInt(line));
-                }
-
-                // Fill the city array
-                if (lineNumber == 1) {
-                    System.arraycopy(line.trim().split(" "), 0, cities, 0, cities.length);
-                }
+            while (reader.hasNextLine()) {
+                reader.nextLine();
+                lineCount++;
             }
 
+            // CLose and reopen the scanner to read the file again
+            reader.close();
+            reader = new Scanner(Paths.get(fileName));
+
+            // Fill the document array
+            document = new String[lineCount];
+
+            for (int i = 0; i < document.length; i++){
+                document[i] = reader.nextLine().trim();
+            }
+            
         } catch (IOException e) {
             System.out.printf("Could not found the file '%s'.", fileName);
         } catch (NumberFormatException e) {
@@ -39,5 +44,29 @@ public class InputReader {
                 reader.close();
             }
         }
+    }
+
+    public static void createCityArray() {
+        // Parse the size of the city array from the first line
+        cities = new String[Integer.parseInt(document[0])];
+
+        // Read the second line into the city array
+        System.arraycopy(document[1].split(" "), 0, cities, 0, cities.length);
+    }
+
+    public static void createRouteArray() {
+        // Parse the size of the route array from the third line
+        routes = new String[Integer.parseInt(document[2])][3]; // City1, City2, Time for every route
+
+        // Fill the routes array
+        for(int i = 0; i < routes.length; i++){
+            System.arraycopy(document[i + 3].split(" "), 0, routes[i], 0, 3);
+        }
+    }
+
+    public static void createQuestionArray() {
+        // Read the last line into the question array
+        question = new String[2]; // Starting city and ending city
+        System.arraycopy(document[document.length - 1].split(" "), 0, question, 0, question.length);
     }
 }
